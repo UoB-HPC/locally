3949348b43b8be994aa5cc9557f3de74303ba282
Tom Lin <tom91136@gmail.com>
2023-04-02 03:50:47 +0100

    Parameterise tests

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,9 @@
 set -eu
 
 dockerfile=$1
+package=$2
+validate=$3
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 extension="${dockerfile##*.}"
 echo "Testing $dockerfile => $extension"
@@ -10,12 +13,12 @@ podman build -t "$extension" -f "$SCRIPT_DIR/$dockerfile" -q .
 podman run --rm --volume "$(readlink -f "$SCRIPT_DIR/../locally.sh"):/usr/bin/locally:z" -i "$extension" /bin/bash -s <<EOF
     set -eu
     whoami
-if command -v make &> /dev/null; then
-    echo "Invalid test, make already exists" && exit 1
+if command -v $package &> /dev/null; then
+    echo "Invalid test, $package already exists" && exit 1
 else
-    locally install make
+    locally install $package
     source locally enable 
-    make --version
+    $validate
 fi
 EOF
 podman rmi "$extension"
